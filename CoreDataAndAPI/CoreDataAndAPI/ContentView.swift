7398fd2a2b99d70ed0286d13af7163fe83b02349
mirellesine <mirellesine@gmail.com>
2023-08-21 17:28:03 -0300

    UI contentView()

--- CoreDataAndAPI/CoreDataAndAPI/ContentView.swift
+++ CoreDataAndAPI/CoreDataAndAPI/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+struct Folder: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
 struct ContentView: View {
+    let folders: [Folder] = [
+            Folder(name: "Folder 1"),
+            Folder(name: "Folder 2"),
+            Folder(name: "Folder 3"),
+            Folder(name: "Folder 4")
+        ]
     var body: some View {
-        VStack {
-            Image(systemName: "teste commit")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            List {
+                ForEach(folders) { folder in
+                    NavigationLink {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "folder")
+                                .foregroundColor(.cyan)
+                                    .font(.title)
+                            VStack(alignment: .leading) {
+                                Text(folder.name)
+                                    .font(.headline)
+                                
+                                Text("descricao")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+                .navigationTitle("Folders")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            //ACAO DO +
+                        } label: {
+                            Label("Add Folder", systemImage: "plus")
+                        }
+                    }
+                }
         }
-        .padding()
     }
 }
 
