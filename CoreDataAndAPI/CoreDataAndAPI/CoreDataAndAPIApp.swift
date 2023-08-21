302887450df62f0bfe1c8d8d7249a45e1b62784b
mirellesine <mirellesine@gmail.com>
2023-08-21 15:27:23 -0300

    adicionando projeto

---
+++ CoreDataAndAPI/CoreDataAndAPI/CoreDataAndAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataAndAPIApp.swift
+//  CoreDataAndAPI
+//
+//  Created by Mirelle Sine on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataAndAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
