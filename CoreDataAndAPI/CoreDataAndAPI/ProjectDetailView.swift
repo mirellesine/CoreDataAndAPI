3f433804548af6b75a6fe5b2a1c52a9131579acc
Thayna <thna.rdg@gmail.com>
2023-08-25 15:20:26 -0300

    app store link added to detail view

--- CoreDataAndAPI/CoreDataAndAPI/ProjectDetailView.swift
+++ CoreDataAndAPI/CoreDataAndAPI/ProjectDetailView.swift
@@ -12,25 +12,79 @@ struct ProjectDetailView: View {
     
     var body: some View {
         VStack {
-            
+            // ícone do projeto
             AsyncImage(url: project.imgIcon) { image in
                 image.resizable()
             } placeholder: {
                 ProgressView()
             }.frame(width: 100, height: 100)
             
+            // nome, turma e descrição
+            VStack {
+                ProjectInfo(project: project)
+            }
+            
+            Divider()
+            
+            // info do CBL
+            VStack {
+                CBLInfo(project: project)
+            }
+            
+            // link app store
+            
+            VStack {
+                AppStoreButton(project: project)
+            }
+            
+            Spacer()
+        }
+        .navigationBarTitle("")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+
+struct ProjectInfo: View {
+    let project: Project
+    var body: some View {
+        VStack {
             Text(project.name).font(.title)
             Text(project.turma).font(.headline)
             Text(project.description)
                 .font(.subheadline)
                 .padding(.vertical)
-            Divider()
+        }
+    }
+}
+
+struct CBLInfo: View {
+    let project: Project
+    
+    var body: some View {
+        VStack {
             Text(project.bigIdea)
             Text(project.challenge)
             Text(project.essentialQuestion)
-            Spacer()
         }
-        .navigationBarTitle("")
-        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AppStoreButton: View {
+    let project: Project
+    
+    var body: some View {
+        VStack {
+            if let url = URL(string: project.link) {
+                Link(destination: url, label: {
+                    Image("AppStoreBadge")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 60)
+                })
+            } else {
+                Text("Invalid Link")
+            }
+        }
     }
 }
